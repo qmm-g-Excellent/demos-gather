9e35564e6f7bf33f5410089f7d25a60c9709ef0b
qmm <qmmhao@sina.com>
2016-07-22 22:32:47 +0800

    clean

--- postcode-shift/barcode--postcode/main/main.js
+++ postcode-shift/barcode--postcode/main/main.js
@@ -4,9 +4,9 @@ function printPostcode(barcode, nativeBarcode, digitsArray) {
     if (str === 'true') {
         let barcodeObj = removeCheckCode(barcode, nativeBarcode);
         let postcodeObj = getPostcode(barcodeObj, nativeBarcode);
-        postcodeObj = checkPostcode(postcodeObj, digitsArray,nativeBarcode);
-        console.log(postcodeObj.str === '条码的校验码正确' ? addSymbol(postcodeObj): postcodeObj.str);
-       return  postcodeObj.str === '条码的校验码正确' ? addSymbol(postcodeObj): postcodeObj.str;
+        postcodeObj = checkPostcode(postcodeObj, digitsArray, nativeBarcode);
+        console.log(postcodeObj.str === '条码的校验码正确' ? addSymbol(postcodeObj) : postcodeObj.str);
+        return postcodeObj.str === '条码的校验码正确' ? addSymbol(postcodeObj) : postcodeObj.str;
     }
     else {
         return str;
@@ -15,14 +15,12 @@ function printPostcode(barcode, nativeBarcode, digitsArray) {
 
 function checkBarcode(barcode) {
     let barcodeArr = barcode.split('');
-
     let newBarcodeArr = barcodeArr.filter(code => code === ':' || '|');
     return newBarcodeArr.join("") === barcode ? 'true' : '条码有非法字符';
 }
 
 function removeCheckCode(barcode, nativeBarcode) {
     let cd = barcode.substring(barcode.length - 1 - nativeBarcode[0].length, barcode.length - 1);
-    console.log(cd);
     let barcodeArr = barcode.split('');
     let length = barcodeArr.length - 1 - nativeBarcode[0].length - 1;
     barcodeArr.splice(0, 1);
@@ -31,52 +29,35 @@ function removeCheckCode(barcode, nativeBarcode) {
 }
 
 function getPostcode(barcodeObj, nativeBarcode) {
-    // let barcodeArr = barcodeObj.barcode.split("").map(code => {
-    //     let index = barcodeObj.barcode.split("").indexOf(code);
-    //     return code.substring(index.indexOf(code), index.indexOf(code) + nativeBarcode[0].length).join("");
-    // });
     let barcode = barcodeObj.barcode.split('');
     let barcodeArr = [];
     for (let i = 0; i < barcode.length; i++) {
         let length = nativeBarcode[0].length;
-        // console.log(barcode.slice(i, i + length).join(""));
         barcodeArr.push(barcode.slice(i, i + length).join(""));
-        i += length -1;
+        i += length - 1;
     }
     let postcode = barcodeArr.map(barcode => {
-      return  nativeBarcode.indexOf(nativeBarcode.find(barcodeStr => barcodeStr === barcode));
-        // console.log(nativeBarcode.indexOf(nativeBarcode.find(barcodeStr => barcodeStr === barcode)));
+        return nativeBarcode.indexOf(nativeBarcode.find(barcodeStr => barcodeStr === barcode));
     }).join('');
     let cd = barcodeObj.cd;
     return {postcode, cd};
 }
 
-function checkPostcode(postcodeObj, digitsArray,nativeBarcode) {
+function checkPostcode(postcodeObj, digitsArray, nativeBarcode) {
     let digits = digitsArray.some(digit => digit === postcodeObj.postcode.length);
-    // console.log(digits ? 'true' : '邮编位数不正确');
     postcodeObj.str = digits ? 'true' : '邮编位数不正确';
-    // console.log(postcodeObj.postcode);
     let sum = postcodeObj.postcode.split('').reduce((prv, cur) => parseInt(prv) + parseInt(cur));
-    
-    let cd = sum % 10 === 0 ? 0 : 10 - sum % 10;
+    let cd = (10 - sum % 10) % 10;
     postcodeObj.str = nativeBarcode.find(barcode => cd === nativeBarcode.indexOf(barcode)) === postcodeObj.cd ? '条码的校验码正确' : '条码的校验码不正确';
-    // console.log(nativeBarcode.find(barcode => cd === nativeBarcode.indexOf(barcode)) );
     return postcodeObj;
 }
 
-function addSymbol(postcodeObj){
-  // console.log(postcodeObj.barcode);
-    
+function addSymbol(postcodeObj) {
     let postcode;
-    if(postcodeObj.postcode.split('').length - 1 > 5 ){
+    if (postcodeObj.postcode.split('').length - 1 > 5) {
         postcode = postcodeObj.postcode.split('');
-        postcode.splice(5,0,"-");
-
+        postcode.splice(5, 0, "-");
     }
-    let a = ['2','4','5'];
-    a.splice(1,0,'wowo');
-    console.log(postcode.join(""));
-
     return postcode.join("");
 }
 
@@ -86,4 +67,4 @@ module.exports = {
     removeCheckCode: removeCheckCode,
     getPostcode: getPostcode,
     checkPostcode: checkPostcode
-}
+};
--- postcode-shift/postcode--barcode/main/main.js
+++ postcode-shift/postcode--barcode/main/main.js
@@ -1,49 +1,39 @@
 'use strict';
 function printBarcode(postcode, nativeBarcode, digitsArray) {
-
     let postcodeObj = checkPostcode(postcode, digitsArray);
     if (postcodeObj.str === "true") {
-        postcodeObj  = getCheckCode(postcodeObj);
-        return getBarcode(postcodeObj,nativeBarcode);
+        postcodeObj = getCheckCode(postcodeObj);
+        return getBarcode(postcodeObj, nativeBarcode);
     }
     return false;
 }
 
 function checkPostcode(postcode, digitsArray) {
-    return digitsArray.some( b => b === (postcode.length -1 )) ? {postcode: postcode, str:"true"} : {postcode: postcode, str: undefined};
+    return digitsArray.some(b => b === (postcode.length - 1 )) ? {
+        postcode: postcode,
+        str: "true"
+    } : {postcode: postcode, str: undefined};
 }
 
 function getCheckCode(postcodeObj) {
-    let postcode = postcodeObj.postcode.replace('-','');
+    let postcode = postcodeObj.postcode.replace('-', '');
     let sum = postcode.split('')
-                      .map(postcode=> {return parseInt(postcode)})
-                      .reduce((prv, cur) => prv + cur);
-    postcodeObj.cd = (sum % 10 === 0) ? 0 : (10 - sum %10);
+        .map(postcode=> {
+            return parseInt(postcode)
+        })
+        .reduce((prv, cur) => prv + cur);
+    postcodeObj.cd = (10 - sum % 10) % 10;
     return postcodeObj;
 }
 
 function getBarcode(postcodeObj, nativeBarcode) {
-    let postcode = postcodeObj.postcode.replace('-', '').split('');
-    postcode = postcode.map(postcode=> {return parseInt(postcode)});
-
-    const cdBarcode = nativeBarcode.find(nativeCode => nativeCode.digit === postcodeObj.cd );
-
-   let codes = postcode.map(code => nativeBarcode.find(nativeCode => nativeCode.digit === code));
-
-    codes.push(cdBarcode);
-    // console.log(codes);
-
-    let  strBarcode =  codes.map(code =>{console.log(code);return code.barcode } );
-    // let strBarcode = [];
-    // for(let code of codes){
-    //  strBarcode.push(code.barcode);
-    // }
-
-    // console.log(strBarcode);
-
-
-    // console.log('|' + strBarcode.join("") + '|');
-
+    let postcode = postcodeObj.postcode.replace('-', '')
+        .split('')
+        .map(postcode=> {return parseInt(postcode)})
+        .map(code => nativeBarcode.find(nativeCode => nativeCode.digit === code));
+    const cdBarcode = nativeBarcode.find(nativeCode => nativeCode.digit === postcodeObj.cd);
+    postcode.push(cdBarcode);
+    let strBarcode = postcode.map(code => code.barcode);
     return '|' + strBarcode.join("") + '|';
 }
 
@@ -52,8 +42,7 @@ module.exports = {
     getCheckCode: getCheckCode,
     checkPostcode: checkPostcode,
     getBarcode: getBarcode
-}
-
+};
 
 
 //总结：includes()方法 适用于数组里面每个元素存放的是字符串，如果是数字类型的而数字，就不能用这个方法，而是用some() 方法
